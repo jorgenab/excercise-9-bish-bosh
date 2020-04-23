5f494b41ad1a6927830c2a8f301305893b25db07
Jörgen Abrahamsson <jorgen@ribbingsfors.se>
2020-04-23 12:58:28 +0200

    Finished Bish-Bosh version 1. (bish_bosh.html and bish_bosh.js)

--- bish_bosh.js
+++ bish_bosh.js
@@ -2,3 +2,39 @@
 //
 // Bish-Bosh Version 1.
 
+function showMessage(id, message){
+	document.getElementById(id).textContent = message;
+}
+
+function divisbleBy(numerator, denominator) {
+	return (numerator % denominator) == 0;
+}
+
+// Does not work
+function bishBosh() {
+	let answer = "";
+	for(let count = 1; count <= 100; count++) {
+		let divisbleBy3 = divisbleBy(count, 3);
+		let divisbleBy4 = divisbleBy(count, 4);
+		if (divisbleBy3 && divisbleBy4) {
+			answer = answer + "Bish-Bosh"
+		}
+		else if (divisbleBy3) {
+			answer = answer + "Bish";
+		}
+		else if (divisbleBy4) {
+			answer = answer + "Bosh";
+		}
+		else {
+			answer = answer + count;
+		}
+		
+		// No comma or space after last number
+		if (count != 100) {
+			answer = answer + ", ";
+		}
+	}
+	return answer;
+}
+
+showMessage("answer", bishBosh());
--- testing.js
+++ testing.js
@@ -1,3 +1,17 @@
 // JavaScript Document
 //
 // Testing misc code
+function showMessage(message){
+	document.getElementById('message').textContent = message;
+}
+
+// alert('Hello, World!');
+// console.log('Till console');
+// console.log('Mer till console');
+
+showMessage('Hello World');
+
+// back-ticks for variabels in strings
+let name = "jorgen";
+let message = `Hello ${name}`;
+showMessage(message);
